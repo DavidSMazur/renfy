2657ae45ed2efbb40ca0255ed7fbf42eff484bf4
David Mazur <davidsmazur1@gmail.com>
2024-02-03 18:05:06 -0500

    add collections

--- Backend/main.py
+++ Backend/main.py
@@ -3,21 +3,40 @@ from fastapi import FastAPI
 import pymongo
 import os
 
-app = FastAPI()
-
-
-client = MongoClient("localhost", 27017 )
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+client = MongoClient("localhost",27017)
 db = client.RENFY
+
+"""
+Givens:
+User {
+  Real Name
+  User_Name
+  Password
+  Email
+  Connections: set(
+)
+}
+{
+  Connection UserName : set()
+  location
+}
+"""
 collection = db.test_collection
 
-db.collection.insertOne(
-    <document>,
-    {
-      writeConcern: <document>
-    }
-)
+def add_user(db,Real_Name,User_Name,Password,Email):
+  collection = db.test_collection
+  new_user= {
+    "Real_Name":Real_Name,
+    "User":User_Name,
+    "Password":Password,
+    "Email":Email,
+    "Connections":[]
+  }
+  collection.insert_one(new_user)
+add_user(db,"David","dmazur","123","Email")
+
+"""
+def add_connection(user, connection, location):
+#also take time
+"""
+
