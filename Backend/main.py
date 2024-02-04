926d04dadacdb78d5bdbccf0f347c3018a2500e3
David Mazur <davidsmazur1@gmail.com>
2024-02-04 11:19:58 -0500

    final

--- Backend/main.py
+++ Backend/main.py
@@ -21,8 +21,8 @@ def add_user(db,Real_Name,User_Name,Password,Email):
       "Email":Email,
       #Differentiating crossings, connection requests, and accepted connections allows for O(1) access. 
       "Crossings":{},  #hash maps of crossed pathes with user 
-      "Connection_Requests":{}, #list
-      "Accepted_Connections":{} #list - mutally accepted
+      "Connection_Requests":{}, 
+      "Accepted_Connections":{} 
     }
     document.insert_one(new_user)
     return 1
@@ -31,29 +31,31 @@ def add_user(db,Real_Name,User_Name,Password,Email):
     return 0
 
 def add_crossings(db,USER_NAME, Connection, Location):
-  if USER_NAME==Connection:
-    print("User can't connect with themselves")
-    return 0
-  document = db.users
-  existing_user = document.find_one({"User": USER_NAME})
-  if existing_user:
-    crossings = existing_user.get("Crossings", {})  # Get existing crossings or create an empty list
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    if Connection in crossings:  
-      crossings[Connection]["count"] += 1 
-      crossings[Connection]["locations"].append(Location)
-      crossings[Connection]["time"].append(current_time)
+  def helper(db,USER_NAME, Connection, Location):
+    if USER_NAME==Connection:
+      print("User can't connect with themselves")
+      return 0
+    document = db.users
+    existing_user = document.find_one({"User": USER_NAME})
+    if existing_user:
+      crossings = existing_user.get("Crossings", {})  # Get existing crossings or create an empty list
+      now = datetime.now()
+      current_time = now.strftime("%H:%M:%S")
+      if Connection in crossings:  
+        crossings[Connection]["count"] += 1 
+        crossings[Connection]["locations"].append(Location)
+        crossings[Connection]["time"].append(current_time)
+      else:
+        crossings[Connection] = {"count": 1, "locations": [Location],"time":[current_time] }
+      update = {"$set": {"Crossings": crossings}}  # Update the crossings list
+      result = document.update_one({"User": USER_NAME}, update)
+
+      return 1  
     else:
-      crossings[Connection] = {"count": 1, "locations": [Location],"time":[current_time] }
-
-    update = {"$set": {"Crossings": crossings}}  # Update the crossings list
-    result = document.update_one({"User": USER_NAME}, update)
-
-    return 1  
-  else:
-    print("Username not found")
-    return 0
+      print("Username not found")
+      return 0
+  helper(db,USER_NAME,Connection,Location)
+  helper(db,Connection,USER_NAME,Location)
 
 def change_pass_w_old(db,USER_NAME,old_password,new_password):
   if old_password==new_password:
